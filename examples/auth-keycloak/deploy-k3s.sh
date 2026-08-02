5afad1b57b3446b7878e7c3b1b0c8d15e1342c15
Jason <vtheanh04@gmail.com>
2026-08-02 15:29:09 +0700

    cập nhật phiên bản

--- examples/auth-keycloak/deploy-k3s.sh
+++ examples/auth-keycloak/deploy-k3s.sh
@@ -6,7 +6,7 @@
 set -e
 
 KUBECONFIG_PATH="./backend/.kube/lens-kubeconfig.yaml"
-VERSION="2.0.15"
+VERSION="2.0.16"
 DOCKER_USER="${DOCKER_USER:-vtaboss}"
 FRONTEND_IMAGE="${FRONTEND_IMAGE:-$DOCKER_USER/greeniq-frontend}"
 BACKEND_IMAGE="${BACKEND_IMAGE:-$DOCKER_USER/greeniq-backend}"
