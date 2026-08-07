69703283134559bd22fb4044a69fc56967847f50
jaivindomadiya <92438349+jaivindomadiya@users.noreply.github.com>
2026-08-07 10:24:12 +0530

    Update fix-pnpm-symlinks.js
    scripts-> fix-pnpm . js wali file edit

--- scripts/fix-pnpm-symlinks.js
+++ scripts/fix-pnpm-symlinks.js
@@ -4,7 +4,7 @@ const path = require("path");
 const content = "#! /usr/bin/env node";
 
 const packageNames = ["cli", "devtools-server"];
-
+const jaivin = "1";
 for (const packageName of packageNames) {
   const distPath = path.resolve(process.cwd(), "packages", packageName, "dist");
   fs.mkdirSync(distPath, { recursive: true });
